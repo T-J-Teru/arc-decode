2683ffb06a1695baae03c92022379ea6f99898c9
Andrew Burgess <andrew.burgess@embecosm.com>
2016-06-09 11:37:03 +0100

    Initial commit of decoder project

---
+++ src/decoder.c
@@ -0,0 +1,9 @@
+int
+main (int argc, char argv[])
+{
+  /* Read bytes off command line and encode them into a buffer.  */
+
+  /* Call the decode library to decode them.  */
+
+  /* Display results.  */
+}
